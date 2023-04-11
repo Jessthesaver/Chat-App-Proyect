f28ea23839e6463056237ecb117b3ae508eca17e
Jessthesaver <jesuscarmona102@gmail.com>
2023-04-11 17:13:46 -0600

    correction in the frontend

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,34 +1,45 @@
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import {createClient} from 'graphql-ws';
-import {GraphQLWsLink} from '@apollo/client/link/subscriptions';
-import {getMainDefinition} from '@apollo/client/utilities';
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
+import { createClient } from "graphql-ws";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const SERVER_URI = "/graphql";
-const WS_URL = `ws://localHost:3000/graphql`;
+const WS_URL =
+  window.location.protocol === "https:"
+    ? `wss://${window.location.host}/graphql`
+    : `ws://${window.location.host}/graphql`;
 
+const httpLink = new HttpLink({
+  uri: SERVER_URI,
+  credentials: "same-origin",
+});
 
-const httpLink=  new HttpLink({
-    uri: SERVER_URI,
-    credentials:'same-origin'
-})
-
-const wsLink = new GraphQLWsLink( createClient({
+const wsLink = new GraphQLWsLink(
+  createClient({
     url: WS_URL,
-    connectionParams:{
-        credentials:'same-origin'
-    }
-}))
+    connectionParams: {
+      credentials: "same-origin",
+    },
+  })
+);
 
-const spitLink = split(({query})=>{
-    const definition=getMainDefinition(query)
-    return (definition.kind==='OperationDefinition' && definition.operation === 'subscription')
-}, wsLink, httpLink)
+const spitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-    link: spitLink,
-    cache: new InMemoryCache({
-        addTypename: false
-    }),
+  link: spitLink,
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 });
 
 export default client;
