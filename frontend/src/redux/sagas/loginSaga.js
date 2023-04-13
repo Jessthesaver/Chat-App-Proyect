36488f9ca626e47432359bcb8f203d54c55f734c
Jessthesaver <jesuscarmona102@gmail.com>
2023-04-13 15:52:13 -0600

    translation error fixed

--- frontend/src/redux/sagas/loginSaga.js
+++ frontend/src/redux/sagas/loginSaga.js
@@ -1,66 +1,66 @@
 import { put, call } from "redux-saga/effects";
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 import client from "../../client";
 import { setUser } from "../reducers/userSlice";
 import { setNotification } from "../reducers/notificationSlice";
+import { t } from "i18next";
 
 function* login(action) {
   const options = {
     mutation: gql`
-        mutation Login($userInput: UserInput) {
-            login(userInput: $userInput) {
+      mutation Login($userInput: UserInput) {
+        login(userInput: $userInput) {
+          username
+          name
+          email
+          avatar
+          friendsList {
+            username
+            name
+            email
+            avatar
+          }
+          requests {
+            from {
               username
               name
-              email
               avatar
-              friendsList {
-                username
-                name
-                email
-                avatar
-              }
-              requests {
-                from {
-                  username
-                  name
-                  avatar
-                }
-                to{
-                  username
-                  name
-                  avatar
-                }
-              }
-              rooms {
-                _id
-                name
-                groupalChat
-                admin {
-                  username
-                } 
-                members {
-                  username
-                  avatar
-                  joinedAt
-                }
-              }
-              settings {
-                language
-              }
-              token
+            }
+            to {
+              username
+              name
+              avatar
             }
           }
-        `,
+          rooms {
+            _id
+            name
+            groupalChat
+            admin {
+              username
+            }
+            members {
+              username
+              avatar
+              joinedAt
+            }
+          }
+          settings {
+            language
+          }
+          token
+        }
+      }
+    `,
     variables: {
       userInput: action.payload.user,
-    }
+    },
   };
   try {
     const res = yield call(client.mutate, options);
     const user = res.data.login;
-  
-    yield put(setUser({ user }));
 
+    yield put(setUser({ user }));
   } catch (error) {
     const notification = {
       error: error.message,
@@ -68,15 +68,15 @@ function* login(action) {
     };
 
     if (notification.error === "User not Found") {
-      notification.error ="userNotFound";
+      notification.error = t("userNotFound");
     } else if (notification.error === "The user or password is incorrect") {
-      notification.error = "userPasswordIncorrect"
+      notification.error = t("userPasswordIncorrect");
     } else if (notification.error === "Missing credentials") {
-      notification.error = "missingCredentials";
+      notification.error = t("missingCredentials");
     }
 
     yield put(setNotification(notification));
   }
-};
+}
 
 export default login;
