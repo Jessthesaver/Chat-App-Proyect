48fe8b86bb39c955247889ddac414dd62a2f7828
Jessthesaver <jesuscarmona102@gmail.com>
2023-04-14 17:48:49 -0600

    translation bugs fixed

--- frontend/src/redux/reducers/notificationSlice.js
+++ frontend/src/redux/reducers/notificationSlice.js
@@ -1,34 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { t } from "i18next";
 
 const initialState = {
-    error: null,
-    severity: null,
-    existence: null,
-    requestAccepted: null,
+  error: null,
+  severity: null,
+  existence: null,
+  requestAccepted: null,
 };
 
 const notificationSlice = createSlice({
-    name: 'login',
-    initialState,
-    reducers: {
-        setNotification: (state = null, action) => {
-            state.error = action.payload.error;
-            state.severity = action.payload.severity;
-        },
-        deleteNotification: (state = null, action) => {
-            state.error = action.payload.error;
-            state.severity = action.payload.error;
-        },
-        setExistence: (state = null, action) => {
-            state.existence = action.payload;
-        },
-        setDefaultNotification: (state = null, action) => {
-            state.error = "defaultError";
-            state.severity = "error";
-        }
+  name: "login",
+  initialState,
+  reducers: {
+    setNotification: (state = null, action) => {
+      state.error = action.payload.error;
+      state.severity = action.payload.severity;
     },
+    deleteNotification: (state = null, action) => {
+      state.error = action.payload.error;
+      state.severity = action.payload.error;
+    },
+    setExistence: (state = null, action) => {
+      state.existence = action.payload;
+    },
+    setDefaultNotification: (state = null, action) => {
+      state.error = t("defaultError");
+      state.severity = "error";
+    },
+  },
 });
 
-export const { setNotification, deleteNotification, setExistence, setDefaultNotification } = notificationSlice.actions;
+export const {
+  setNotification,
+  deleteNotification,
+  setExistence,
+  setDefaultNotification,
+} = notificationSlice.actions;
 
 export default notificationSlice.reducer;
--- frontend/src/redux/sagas/signupSaga.js
+++ frontend/src/redux/sagas/signupSaga.js
@@ -1,41 +1,43 @@
 import { put, call } from "redux-saga/effects";
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 import client from "../../client";
 import { setSignupFetching } from "../reducers/signupSlice";
 import { setNotification } from "../reducers/notificationSlice";
+import { t } from "i18next";
 
 function* signup(action) {
-    const options = {
-        mutation: gql`
-        mutation CreateUser($userInput: UserInput) {
-            createUser(userInput: $userInput) {
-              success
-              errorMessage
-              value {
-                username
-                name
-                email
-              }
-            }
+  const options = {
+    mutation: gql`
+      mutation CreateUser($userInput: UserInput) {
+        createUser(userInput: $userInput) {
+          success
+          errorMessage
+          value {
+            username
+            name
+            email
           }
-        `,
-        variables: {
-            userInput: action.payload.signup,
         }
-    };
+      }
+    `,
+    variables: {
+      userInput: action.payload.signup,
+    },
+  };
 
-    try {
-        yield put(setSignupFetching());
-        yield call(client.mutate, options);
+  try {
+    yield put(setSignupFetching());
+    yield call(client.mutate, options);
 
-        yield put(setNotification({
-            error: "user Created",
-            severity: "success"
-        }));
-    } catch (error) {
-        yield put(setNotification({ error: error.message, severity: "error" }));
-    }
-
-};
+    yield put(
+      setNotification({
+        error: t("userCreated"),
+        severity: "success",
+      })
+    );
+  } catch (error) {
+    yield put(setNotification({ error: error.message, severity: "error" }));
+  }
+}
 
 export default signup;
